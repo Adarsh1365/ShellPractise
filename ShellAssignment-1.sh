fa7449d10d6a441525b8d8d328fad6fc3c8a4191
Adarsh <adarshupputuri2@gmail.com>
2026-05-27 21:22:11 +0530

    Commiting new changes

--- ShellAssignment-1.sh
+++ ShellAssignment-1.sh
@@ -2,7 +2,7 @@
 
 echo "Hello, World!"
 echo "Today is $(date)"
-echo "Current user: $(id)"
+echo "Current user: $(id -u)"
 echo "Current directory: $(pwd)"
 echo "Files in current directory:"
 ls -l
