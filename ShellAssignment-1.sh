40df8ab2398a79e68ba5ec4bfe8cf0d3b5714f12
Adarsh <adarshupputuri2@gmail.com>
2026-05-27 21:31:04 +0530

    Commiting new changes

--- ShellAssignment-1.sh
+++ ShellAssignment-1.sh
@@ -3,6 +3,12 @@ Mentor=$1
 echo "Hello, World!"
 echo "Today is $(date)"
 echo "Current user: $(id -u)"
-echo "Current directory: $(pwd)"
-echo "Files in current directory:"
+#echo "Current directory: $(pwd)"
+#echo "Files in current directory:"
 echo "$Mentor is the mentor"
+echo "$Mentor : please enter your username"
+read username
+echo "$username : please enter your password"
+read -s password
+echo "Username: $username"
+echo "Password: $password"
