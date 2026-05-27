d83fd2e1b793e2dac93997e1dbe1e862509f1a09
Adarsh <adarshupputuri2@gmail.com>
2026-05-27 21:25:07 +0530

    Commiting new changes

--- ShellAssignment-1.sh
+++ ShellAssignment-1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+Mentor="Adarsh"
 echo "Hello, World!"
 echo "Today is $(date)"
 echo "Current user: $(id -u)"
 echo "Current directory: $(pwd)"
 echo "Files in current directory:"
-ls -l
+echo "$Mentor is the mentor"
