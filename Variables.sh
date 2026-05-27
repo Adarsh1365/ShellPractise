2e94aad37700f6af9457c22a533185414fe2122e
Adarsh <adarshupputuri2@gmail.com>
2026-05-27 21:47:40 +0530

    Commiting new changes

---
+++ Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables passed to script $@"
+
+echo "Total number of variables passed to script $#"
+echo "script name is $0"
+echo "First variable passed to script $1"
+echo "Second variable passed to script $2"
+echo "user name $USER"
