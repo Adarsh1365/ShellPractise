a9908bd50cd9c936845ecb614d1583eb83005a6f
Adarsh <adarshupputuri2@gmail.com>
2026-05-28 16:12:24 +0530

    new commit

--- MySQL.sh
+++ MySQL.sh
@@ -4,7 +4,10 @@ LogDir="/home/ec2-user/shell-logs"
 Logfile="$LogDir/MysqlInstallingLog.log"
 
 User=$(id -u)
-
+if [ $User -ne 0 ]; then
+  echo "Please run using root acess"
+  exit 1
+fi
 Validate()
 {
   if [ $1 -ne 0 ] ; then
@@ -15,17 +18,14 @@ Validate()
   fi
 }
 
-if [ $User -ne 0 ]; then
-  echo "Please run using root acess"
-  exit 1
-fi
+
 
 dnf list installed mysql &>> $Logfile
 
 if [ $? -eq 0 ] ; then
   echo "Mysql is already is installed....skipping now"
 else
-  echo "Installing mysql now"
+  echo "Installing mysql-server now"
   dnf install mysql-server -y &>> $Logfile
   Validate $? "Mysql installing"
 
