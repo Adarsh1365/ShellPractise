d8097a580ac8eb95a3d105c2f13de3eeeaadc62e
Adarsh <adarshupputuri2@gmail.com>
2026-05-28 18:04:43 +0530

    new commit

--- dynamicinstallation.sh
+++ dynamicinstallation.sh
@@ -2,15 +2,21 @@
 
 LogDir="/home/ec2-user/shell-logs"
 Logfile="$LogDir/mysql.log"
+TIMESTAMP= $("date +%y-%m-%d %H-%M-%S" )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[30m"
+
 USER=$(id -u)
 
 if [ $USER -ne 0 ]; then
-    echo "please run the script using root access"
+    echo $TIMESTAMP $R [ERROR] $N "please run the script using root access" | tee -a $Logfile
     exit 1
 fi
 
 if [ $# -eq 0 ]; then
-   echo "Please provide softwares to install as a parameters"
+   echo $TIMESTAMP $R [ERROR] $N "Please provide softwares to install as a parameters" | tee -a $Logfile
    echo "USAGE command sh $0 <software-1> <software-2>"
    exit 1
 fi
@@ -18,10 +24,10 @@ fi
 validate()
 {
   if [ $1 -ne 0 ]; then
-    echo "$2 is FAILED" | tee -a $Logfile
+    echo $TIMESTAMP $R [ERROR] $N "$2 is $R FAILED " | tee -a $Logfile
     exit 1
   else
-    echo "$2 is SUCESS" | tee -a $Logfile
+    echo $TIMESTAMP $G [SUCCESS] $N "$2 is $G SUCESS" | tee -a $Logfile
   fi
 }
 
@@ -37,9 +43,9 @@ do
       systemctl start mysqld
       validate $? "$software starting"
     else
-      echo "$software already installed...SKIPPNG" | tee -a $Logfile
+      echo "$software already installed...$Y SKIPPNG" | tee -a $Logfile
     fi
 
 done
 
-echo "Script ended running in $SECONDS"
+echo "Script ended running in $SECONDS seconds"
