a9f98d9126ef73aeb451f77edb1d3c12ea1f6eeb
Adarsh <adarshupputuri2@gmail.com>
2026-05-28 17:13:22 +0530

    new commit

--- dynamicinstallation.sh
+++ dynamicinstallation.sh
@@ -4,12 +4,12 @@ LogDir="/home/ec2-user/shell-logs"
 Logfile="$LogDir/mysql.log"
 USER=$(id -u)
 
-if [ $USER -ne 0 ] ; then
+if [ $USER -ne 0 ]; then
     echo "please run the script using root access"
     exit 1
 fi
 
-if [ $# eq 0] ; then
+if [ $# eq 0]; then
    echo "Please provide softwares to install as a parameters"
    echo "USAGE command sh $0 <software-1> <software-2>"
    exit 1
@@ -28,7 +28,7 @@ validate()
 for software in $@
 do
     dnf list installed $software &>> $Logfile
-    if[ $? -ne 0] ; then
+    if[ $? -ne 0]; then
       dnf install $software -y  &>> $Logfile
       validate $? "$software installation"
       systemctl enable mysqld &>> $Logfile
