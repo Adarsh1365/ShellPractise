5777f7aff9c393b7d3b9fd12fd044b99a23cec1e
Adarsh <adarshupputuri2@gmail.com>
2026-05-30 16:59:23 +0530

    new commit

--- shell-roboshop/launchinstances-v2.sh
+++ shell-roboshop/launchinstances-v2.sh
@@ -36,10 +36,12 @@ fi
 Action=$1
 shift
 
-Get_Instanceid=$(aws ec2 describe-instances \
+Get_Instanceid(){
+aws ec2 describe-instances \
                      --filters "Name=tag:Name,Values=roboshop-$1" "Name=instance-state-name,Values=running" \
                      --query "Reservations[].Instances[].InstanceId" \
                      --output text)
+}
 
 for instance in $@
 do
